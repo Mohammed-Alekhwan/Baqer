c1ac472767c5b76c42957bdb1ae6ed5697e06d61
Mohammed <mmhak@hotmail.com>
2023-07-24 01:14:05 +0300

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,33 @@
 require('dotenv').config()
+
 const express = require('express')
-
 const app = express()
+const mongooes = require('mongoose')
+const workoutRouter = require('./routes/workout')
 
-app.get('/', (req,res) => {
-    res.json({msg:"Welcome to Baqer"})
+
+// middleware
+
+app.use(express.json())
+
+app.use((req,res,next) =>{
+    console.log(req.path,req.method)
+    next()
 })
 
-app.listen(process.env.PORT, ()=>{
+// Route
+app.use('/api/workouts',workoutRouter)
+
+// connect to db
+mongooes.connect(process.env.MONGO_URI)
+.then(()=> {
+    // listen for port
+    app.listen(process.env.PORT, ()=>{
     console.log("Listening for port ", process.env.PORT)
 })
+})
+
+.catch((error) =>{
+    console.log(error)
+})
+
